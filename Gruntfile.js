572ef3c1437a3ec371a9cef91590d5a3e9955642
nanomishka <mihail777str@yandex.ru>
2015-02-21 14:22:23 +0300

    Fest is working

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,10 +5,28 @@ module.exports = function (grunt) {
                 command: 'java -cp L1.2-1.0-jar-with-dependencies.jar main.Main 7070'
                 /* запуск сервера */
             }
-        } 
+        },
+        fest: { /* grunt-fest */
+            templates: { /* Подзадача */
+                files: [{
+                    expand: true,
+                    cwd: 'templates', /* исходная директория */
+                    src: '*.xml', /* имена шаблонов */
+                    dest: 'public_html/js/tmpl' /* результирующая директория */
+                }], 
+	            template: function (data) { /* формат функции-шаблона */
+	                return grunt.template.process(
+	                    /* присваиваем функцию-шаблон переменной */
+	                    'var <%= name %>Tmpl = <%= contents %> ;',
+	                    {data: data}
+	                );
+	            }
+            }
+        }
     });
  
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-fest');
     //grunt.registerTask('default', ['concat']);
 };
         
